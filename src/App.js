8e07717dfa6553a3bda1d1dbc0b4babfc129f88a
Akhil Augustin <akhiljw46@gmail.com>
2023-03-27 21:01:13 +0530

    Created Chat container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import ChatContainer from './components/chat/ChatContainer';
 import Header from './components/layout/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <ChatContainer />
     </>
   );
 }
---
+++ src/components/chat/ChatContainer.js
@@ -0,0 +1,11 @@
+import classes from './ChatContainer.module.css';
+
+const ChatContainer = () => {
+  return (
+    <section className={classes.container}>
+      <p>HEllo</p>
+    </section>
+  );
+};
+
+export default ChatContainer;
