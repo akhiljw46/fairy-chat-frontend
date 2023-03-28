dec743979655e2c9a84993c719efa761150a7670
Akhil Augustin <akhiljw46@gmail.com>
2023-03-28 08:06:14 +0530

    Testing with dummy messages

--- src/components/chat/ChatContainer.js
+++ src/components/chat/ChatContainer.js
@@ -2,14 +2,41 @@ import ChatBubble from './ChatBubble';
 import classes from './ChatContainer.module.css';
 import ChatMessages from './ChatMessages';
 
+const DummyMessages = [
+  {
+    id: 'm1',
+    isUser: true,
+    messageText: 'Hello',
+  },
+  {
+    id: 'm2',
+    isUser: false,
+    messageText: 'Yes, how can I help you?',
+  },
+  {
+    id: 'm3',
+    isUser: true,
+    messageText: 'Can you tell me who is Akhil?',
+  },
+  {
+    id: 'm4',
+    isUser: false,
+    messageText: 'Akhil is a boy who lives in his own world!',
+  },
+];
+
 const ChatContainer = () => {
+  const messages = DummyMessages.map((message) => (
+    <ChatBubble
+      key={message.id}
+      text={message.messageText}
+      isUser={message.isUser}
+    />
+  ));
+
   return (
     <section className={classes.container}>
-      <ChatMessages>
-        <ChatBubble text="Hello" isUser={true} />
-        <ChatBubble text="How are you" isUser={false} />
-        <ChatBubble text="I am fine, thank you" isUser={true} />
-      </ChatMessages>
+      <ChatMessages>{messages}</ChatMessages>
     </section>
   );
 };
