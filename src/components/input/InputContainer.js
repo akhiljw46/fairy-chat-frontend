00af4051f8c16d291ef442816a4df421efb50de4
Akhil Augustin <akhiljw46@gmail.com>
2023-03-28 23:54:23 +0530

    Testing to fetch data

--- src/components/input/InputContainer.js
+++ src/components/input/InputContainer.js
@@ -1,4 +1,4 @@
-import { useContext, useRef } from 'react';
+import { useContext, useRef, useState } from 'react';
 
 import MessageContext from '../utils/message-context';
 import InputText from './InputText';
@@ -10,15 +10,45 @@ const InputContainer = () => {
   const inputRef = useRef(null);
   const messageCtx = useContext(MessageContext);
 
-  const submitHandler = (event) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMessage = async (message) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        'https://fairy-chat.cyclic.app/v1/message/',
+        {
+          method: 'POST',
+          body: JSON.stringify({ message }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      setIsLoading(false);
+      return data;
+    } catch (error) {}
+  };
+
+  const submitHandler = async (event) => {
     event.preventDefault();
+    const messageText = inputRef.current.value;
 
     messageCtx.addMessage({
       id: Date.now(),
       isUser: true,
-      messageText: inputRef.current.value,
+      messageText,
     });
     inputRef.current.value = '';
+    console.log(await fetchMessage(messageText));
   };
 
   return (
