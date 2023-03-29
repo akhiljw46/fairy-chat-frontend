9450b27e574d379e3e40131427dbec7803e71496
Akhil Augustin <akhiljw46@gmail.com>
2023-03-30 01:43:31 +0530

    Fixed paragraph rendering

--- src/components/chat/ChatBubble.js
+++ src/components/chat/ChatBubble.js
@@ -1,13 +1,17 @@
 import classes from './ChatBubble.module.css';
 
 const ChatBubble = (props) => {
+  const messageContent = props.text
+    .split('\n')
+    .map((para) => <p key={Math.random()}>{para}</p>);
+
   return (
     <li
       className={`${classes.bubble} ${
         props.isUser ? classes.user : classes.ai
       }`}
     >
-      <p>{props.text}</p>
+      <div className={classes['out-para']}>{messageContent}</div>
     </li>
   );
 };
