5d89cda534ffe0a0e16ce59fe0e72c0ffb6cd742
Akhil Augustin <akhiljw46@gmail.com>
2023-03-30 08:42:03 +0530

    Added placeholder to input textfield

--- src/components/input/InputText.js
+++ src/components/input/InputText.js
@@ -2,7 +2,14 @@ import classes from './InputText.module.css';
 import { forwardRef } from 'react';
 
 const InputText = forwardRef((props, ref) => {
-  return <input ref={ref} className={classes.input} type="text" />;
+  return (
+    <input
+      ref={ref}
+      className={classes.input}
+      type="text"
+      placeholder="Type here..."
+    />
+  );
 });
 
 export default InputText;
